b31da839e07e106f7155c6b03a25e3185c968bc7
Ali Sultan <sasultan786@gmailc.om>
2026-05-29 21:48:22 +0500

    Added Monte Carlo Style averaging for multiple paths and option pricing model to existing framework

--- GBM-sim/gbm_simulation.py
+++ GBM-sim/gbm_simulation.py
@@ -22,7 +22,7 @@ def fetch_inputs_stock(ticker):
     S0 = closing_prices.iloc[-1] #Price at the last date
 
     return mean_ann, sigma_ann, S0
- 
+
 def gbm_func(price,r,sigma,dt,z):
     return price*np.exp((r-(1/2)*sigma**2)*dt+sigma*np.sqrt(dt)*z)
 
@@ -40,34 +40,59 @@ if choice== "1":
     sigma= float(input("Volatility (Sigma): "))
     r= float(input("Risk-free rate: "))
     time= int(input("Time Period: "))
+    option_type= input("Option Type(call or put): ")
+    k= float(input("Strike Price: "))
 
 elif choice== "2":
     while True:
         try:
             input_ticker= input('Please enter the ticker for the stock (e.g UBL.KA for united bank limited): ')
-            r,sigma,S0 = fetch_inputs_stock(input_ticker)
+            mu,sigma,S0 = fetch_inputs_stock(input_ticker)
             break
         except:
             print('Invalid Ticker. Please try again.')
+    option_type= input("Option type(call or put): ")
+    k= float(input("Strike Price: "))
     time= int(input("Time Period: "))
+
+
+r = 0.158# taken from investing.com (https://www.investing.com/rates-bonds/pakistan-1-year-bond-yield)
+k= 110
 
 n= 252 #Time Steps
 dt=time/n
+m=1000  #Number of Paths
 
 #Initialising Grids i.e X and Y Axis
-time_grid= np.linspace(0,time,n+1) #X axis for graph
-stock_path= np.zeros(n+1) #Stock Path Array
+time_grid= np.linspace(0,time,n) #X axis for graph
+stock_path= np.zeros((m,n)) #Stock Path Array
+payoff_mat= np.zeros(m)
 
-stock_path[0]= float(S0) #Initial Value is set to Current Stock Price
+stock_path[:,0]= float(S0) #Initial Value is set to Current Stock Price
 
 #Main Loop
 
-for i in range(n):
-    z= np.random.normal()
-    stock_path[i+1]= gbm_func(stock_path[i],r,sigma,dt,z)
+for i in range(n-1):
+    z= np.random.normal(size=m)
+    stock_path[:,i+1]= gbm_func(stock_path[:,i],r,sigma,dt,z)
 
-# Making Plot
-plt.plot(time_grid,stock_path)
+
+if option_type=='call':
+    payoff_mat= np.maximum(stock_path[:,n-1]-k,0)
+    payoff= payoff_mat.mean()
+elif option_type=='put':
+    payoff_mat= np.maximum(k-stock_path[:,n-1],0)
+    payoff= payoff_mat.mean()
+
+option_price= payoff*np.exp(-r*(time))
+
+terminal_price= np.mean(stock_path[:,-1])
+print("The expected stock price after",time,f"years is {terminal_price:.2f}")
+print(f"The price of the {option_type} on the stock is {option_price:.2f}")
+
+# Plotting All Paths
+
+plt.plot(time_grid,stock_path.T)
 plt.title("Simulated Stock Price",fontsize=16)
 plt.xlabel("Time",fontsize=12)
 plt.ylabel("Stock Price",fontsize=12)
